beed66d750493a8a79106a21979c70bfa1ddf4fc
Prasanths17 <prass1709001@gmail.com>
2024-03-13 17:47:12 +0530

    new changes

--- Routes/route.js
+++ Routes/route.js
@@ -1,3 +1,103 @@
 const express = require('express');
-const app = express();
+const router = express.Router();
 
+const {createPool} = require('mysql2');
+const pool = createPool({
+    host:"localhost",
+    user:"root",
+    password:"Prasanths@2001",
+    database: "novastrid",
+    connectionLimit: 10
+})
+
+
+router.get('/employeeInfo' , (req,res) => {
+    pool.query(`select * from employeeInfo as ei join employeeSalary as es on ei.employee_id=es.employee_id` , (err , result) => {
+        if(err){
+            console.log(err)
+            res.status(500).send(`Error in retriving employeeInfo`);
+        }
+        else{
+            res.status(200).json(result);
+        }
+    });
+});
+
+
+router.get('/employeeInfo/:id' , (req,res) => {
+    const inputId = req.params.id;
+    pool.query(`select * from employeeInfo as ei 
+                join 
+                employeeSalary as es 
+                on ei.employee_id=es.employee_id
+                where ei.employee_id=${inputId}` , (err , result) => {
+                        
+                    if(err){
+                        console.log(err);
+                        res.status(500).send(`Requested id is not present in the database`);
+                    }else if(result.length == 0){
+                        res.status(400).send(`Invalid employee_id`);
+                    }else{
+                        res.status(200).json(result);
+                    }
+                })
+})
+
+router.post('/employeeInfo' , (req,res) => {
+    
+
+    const {employee_id,employee_name,Address,job_title,city,salary} = req.body;
+    console.log(req.body);
+    pool.query(`insert into employeeInfo values (?,?,?,?,?)`, [employee_id,employee_name,Address,job_title,city] , (err,result) => {    
+        if(err){
+            console.log(err);
+            res.status(500).send(`Error in creating`);
+            return;
+        }
+        pool.query(`insert into employeeSalary values (?,?)`,[employee_id,salary] , (err,result) => {
+            if(err){
+                console.log(err);
+                res.status(500).send(`Error in creating`);
+            }else{
+                res.status(200).send(`Employee is successfully created`);
+            }
+        })
+    })
+})
+
+router.put('/employeeInfo/:id' , (req,res) => {
+    const inputId = req.params.id;
+    const {Address,city,salary} = req.body;
+    pool.query(`update employeeInfo as ei join employeeSalary as es 
+                set ei.Address=? , ei.city=? , es.salary=?
+                where ei.employee_id=${inputId}`, [Address,city,salary] , (err,result) => {
+                    if(err){
+                        console.log(err);
+                        res.status(500).send(`Error in updating the details`);
+                        
+                    }else if(result.affectedRows==0){
+                        console.log("Invalid id");
+                        res.status(404).send(`Give valid id`);
+                    }else{
+                        //res.json(result);
+                        res.status(200).send(`Employee details are successfully updated`);
+                    }
+                })
+})
+
+router.delete('/employeeInfo/:id' , (req,res) => {
+    const inputId = req.params.id;
+    pool.query(`delete from employeeInfo where employee_id=${inputId}` , (err,result) => {
+        if(err){
+            console.log(err);
+            res.status(500).send(`Error in deleting the data`);
+        }else if(result.affectedRows==0){
+            console.log("Invalid id");
+            res.status(404).send(`Give valid id`);
+        }else{
+            res.status(200).send('Employee details successfully deleted');
+        }
+    })
+})
+
+module.exports = router;
